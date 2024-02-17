f684ea021df03b86b5241678efffec1fe4a51d5c
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-17 18:36:42 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240217/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void print_5_stars(){
+    cout << "**********" << endl;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int i = 0; i<5; i++){
+        print_5_stars();
+    }
+    return 0;
+}
