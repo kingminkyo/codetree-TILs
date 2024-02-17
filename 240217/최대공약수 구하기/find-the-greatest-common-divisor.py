b959cf754528aa3523e85b06869cc4af9f947db8
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-17 19:09:36 +0900

    Title: 최대공약수 구하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240217/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240217/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,13 +1,25 @@
-a, b = int(input().split())
+# a, b = input().split()
 
-if a>=b:
-    real = a; # 12, 18인 경우 18 
-else:
-    real = b; 
+# if a>=b:
+#     real = a; # 12, 18인 경우 18 
+# else:
+#     real = b; 
 
-for i in real:
-    if (real%i == 0) and (b%i == 0):
-        real_num = i
+# for i in real:
+#     if (real%i == 0) and (b%i == 0):
+#         real_num = i
 
+
+# print(real_num)
+
+
+a, b = map(int, input().split())  # 입력 값을 정수로 변환
+
+def gcd(x, y):
+    while(y):
+        x, y = y, x % y # 12, 18인 경우 y = 18, x 12를 18로 
+    return x
+
+real_num = gcd(a, b)  # gcd 함수를 사용하여 최대공약수를 계산
 
 print(real_num)
