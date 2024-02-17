4529ad6e2395f53ad03df8a40787005e5baeaa88
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-17 18:58:11 +0900

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240217/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240217/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,12 +1,12 @@
 a, b = input().split()
 
-if a>b:
-    real = a;
+if a>=b:
+    real = a; # 12, 18인 경우 18 
 else:
-    real = b;
+    real = b; 
 
 for i in real:
-    if real%i and b%i :
+    if (real%i == 0) and (b%i == 0):
         real_num = i
 
 
