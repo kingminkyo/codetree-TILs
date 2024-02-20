c637852c59555f819767fe1de765bc9430395619
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-20 18:13:19 +0900

    Title: 두 수의 거듭제곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240220/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int gudub (int x, int y){
+
+    return x ^ y
+
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    int a, b = 0;
+
+
+    cin >> a;
+    cin >> b;
+    
+    
+    print (gudub(a, b))
+
+    return 0;
+}
