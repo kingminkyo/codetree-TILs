8013d1587debc8803155af9e1bc10405db63b9e3
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-21 20:12:15 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240221/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240221/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -4,14 +4,15 @@ a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
 def isSq(a, b, n1, n2):
-    result = 0
-    for i in range (n1):
+    
+    for i in range (n1-n2):
         result = 0
         for j in range (n2):
             if a[i+j] == b[j]:
-                result += 1
+                result +=1
                 if result == n2:
                     return True
+                
         
     return False
 
