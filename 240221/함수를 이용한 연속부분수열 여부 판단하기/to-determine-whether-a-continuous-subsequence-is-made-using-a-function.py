7bc0fb5e3d684641cfccd4fbf0121318e3d0a127
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-21 19:57:27 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240221/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,17 @@
+n1, n2 = tuple(map(int, input().split()))
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+def isSq(a, b, n1, n2):
+    for i in range (n1):
+        for j in range (n2):
+            if a[i+j] == b[j]:
+                return True
+        
+    return False
+
+if isSq(a, b, n1, n2):
+    print("Yes")
+else:
+    print("No")
