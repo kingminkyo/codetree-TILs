647c138f5191ea3481ceeebaef2a975b83a83b9c
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-22 21:44:25 +0900

    Title: 특정 구간의 합, Time: 203ms, Memory: 28MB, Status: Passed - CodeTree

--- 240222/특정 구간의 합/sum-of-specific-intervals.py
+++ 240222/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,6 +1,21 @@
+# n, m = tuple(map(int, input().split()))
+# A = list(map(int,input().split()))
+# a[][] = list
+# for i in range(m):
+#     a[m] = list(map(int,input().split()))
+
+
 n, m = tuple(map(int, input().split()))
-A = list(map(int,input().split()))
-a[m][2]
+A = list(map(int, input().split()))
+a = []  # 이중 리스트 초기화
 for i in range(m):
-    for j in range(2):
-        a[m][j] = int(input())
+    a.append(list(map(int, input().split())))  # 각 입력을 리스트로 변환하여 추가
+
+
+for i in range(m):
+    result = 0
+    for j in range(a[i][0], a[i][1]+1):
+        # print(j)
+        result += A[j-1]
+
+    print(result)
