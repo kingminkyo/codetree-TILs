7057492223d28a81048c3787095523c37a50629d
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-24 19:56:25 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240224/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
@@ -0,0 +1,15 @@
+a = int(input())
+
+def fun(n):
+    if n == 1:
+        
+        return 0
+
+    if n % 2 == 0:
+        return fun(n//2) + 1
+
+    else:
+        return fun(n*3 + 1) + 1
+
+
+print(fun(a))
