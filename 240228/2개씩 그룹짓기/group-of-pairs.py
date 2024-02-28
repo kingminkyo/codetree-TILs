bf25d707f75f0fc501da7097698e6812fb8a5bff
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-28 23:04:32 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240228/2개씩 그룹짓기/group-of-pairs.py
+++ 240228/2개씩 그룹짓기/group-of-pairs.py
@@ -16,11 +16,11 @@ for i in range(n):
         group_b.append(arr[n-i])
 
 for i in range(n, n*2):
-    print(i)
+    # print(i)
     if i % 2 == 0 or i == 0:
-        group_a.append(arr[n-i])
-    else:
         group_b.append(arr[i])
+    else:
+        group_a.append(arr[n-1])
 
 result = 0
 for i in range(n):
