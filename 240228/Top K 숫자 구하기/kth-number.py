969d9cf9b69682686ad52369f3c2d503deb110df
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-28 22:25:17 +0900

    Title: Top K 숫자 구하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/Top K 숫자 구하기/kth-number.py
+++ 240228/Top K 숫자 구하기/kth-number.py
@@ -1,6 +1,4 @@
-nk = list(map(int, input().split()))
-n = nk[0]
-k = nk[1]
+n, k = tuple(map(int,input().split()))
 arr = list(map(int, input().split()))
 arr_sorted = sorted(arr)
 print(arr_sorted[k-1])
