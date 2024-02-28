c41ca5f37c28ab74915b42173071cee62d1b4a3a
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-02-28 23:02:12 +0900

    Title: 2개씩 그룹짓기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240228/2개씩 그룹짓기/group-of-pairs.py
+++ 240228/2개씩 그룹짓기/group-of-pairs.py
@@ -8,17 +8,23 @@ group_b = []
 
 # print(arr)
 
-for i in range(n*2):
+for i in range(n):
     # print(i)
     if i % 2 == 0 or i == 0:
         group_a.append(arr[i])
     else:
-        group_b.append(arr[n*2-i])
+        group_b.append(arr[n-i])
 
+for i in range(n+1, n*2):
+    # print(i)
+    if i % 2 == 0 or i == 0:
+        group_a.append(arr[i])
+    else:
+        group_b.append(arr[n-i])
 
 result = 0
 for i in range(n):
     result += group_b[i]
 
 
-print(group_b)
+print(result)
