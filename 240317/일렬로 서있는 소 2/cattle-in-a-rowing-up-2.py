236799b0a2147ec1c0988a29cd7c4f15d685fa9c
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-17 01:36:18 +0900

    Title: 일렬로 서있는 소 2, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240317/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240317/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -6,9 +6,9 @@ count = 0
 for i in range(n):
 
     for j in range(i+1, n):
-        if a[i] < a[j]:
+        if a[i] <= a[j]:
             for k in range(j+1, n):
-                if a[j] < a[k]:
+                if a[j] <= a[k]:
                     count +=1
 
 
