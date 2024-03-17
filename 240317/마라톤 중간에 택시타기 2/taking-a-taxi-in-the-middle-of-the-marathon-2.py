af9ab2963afe73166316181de685f3187086a7a2
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-17 11:01:10 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240317/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240317/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -5,23 +5,25 @@ n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 result = sys.maxsize
-dist = 0
+
+
 for i in range (1, n-1): # 1~2
+    dist = 0
     x1 = arr[0][0]
     y1 = arr[0][1]
     for j in range(1, n): # 1~3 
-         
-
-        if j != i:
-            dist += (abs(arr[j][0]-x1) + abs(arr[j][1]-y1))    
-            x1 = abs(arr[j][0])
-            y1 = abs(arr[j][1])
+        if j == i:
+            continue
+        dist += (abs(arr[j][0]-x1) + abs(arr[j][1]-y1))    
+        x1 = abs(arr[j][0])
+        y1 = abs(arr[j][1])
         
         # print(x1, y1)
 
-    if dist < result:
+    if dist <= result:
         result = dist
+    # ans = min(dist, result)
 
-    dist = 0
+    
 
 print(result)
