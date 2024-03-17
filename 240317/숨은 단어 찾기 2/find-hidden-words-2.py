b552e368b23b8f56d9187790833245139130ec64
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-17 17:35:54 +0900

    Title: 숨은 단어 찾기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240317/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 240317/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -8,17 +8,17 @@ for i in range(n):
         if arr[i][j] =="L":
             if n > 3:
 
-                if j < n-3 and arr[i][j+1] == "E" and arr[i][j+2] == "E" : count+=1 ; 
                 if i < n-3 and j < n-3 and arr[i+1][j+1] == "E" and arr[i+2][j+2] == "E" : count+=1; 
                 if i < n-3 and j>=2 and arr[i+1][j-1] == "E" and arr[i+2][j-2] == "E" : count+=1; 
                 if i >=2 and j>=2 and arr[i-1][j-1] == "E" and arr[i-2][j-2] == "E" : count+=1 ; 
                 if i >= 2 and j < n-3 and arr[i-1][j+1] == "E" and arr[i-2][j+2] == "E" : count+=1;
-                if j >= 2 and arr[i][j-1] == "E" and arr[i][j-2] == "E" : count+=1 ; 
 
-            if m > 3:
                 if i < n-3 and arr[i+1][j] == "E" and arr[i+2][j] == "E" : count+=1 ; 
                 if i >= 2  and arr[i-1][j] == "E" and arr[i-2][j] == "E" : count+=1; 
+            if m > 3:
 
+                if j >= 2 and arr[i][j-1] == "E" and arr[i][j-2] == "E" : count+=1 ; 
+                if j < n-3 and arr[i][j+1] == "E" and arr[i][j+2] == "E" : count+=1 ; 
                 
 
 
