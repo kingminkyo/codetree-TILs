fc0cb78efb60354c7dfee27924392bfbf1e77972
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-17 17:29:26 +0900

    Title: 숨은 단어 찾기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240317/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -0,0 +1,28 @@
+n, m = tuple(map(int, input().split()))
+arr = [input() for _ in range(n)]
+
+count = 0 
+
+for i in range(4):
+    for j in range(6):
+        if arr[i][j] =="L":
+            if j < n-3 and arr[i][j+1] == "E" and arr[i][j+2] == "E" : count+=1 ; 
+            if i < n-3 and j < n-3 and arr[i+1][j+1] == "E" and arr[i+2][j+2] == "E" : count+=1; 
+            if i < n-3 and arr[i+1][j] == "E" and arr[i+2][j] == "E" : count+=1 ; 
+            if i < n-3 and j>=2 and arr[i+1][j-1] == "E" and arr[i+2][j-2] == "E" : count+=1; 
+            if j >= 2 and arr[i][j-1] == "E" and arr[i][j-2] == "E" : count+=1 ; 
+            if i >=2 and j>=2 and arr[i-1][j-1] == "E" and arr[i-2][j-2] == "E" : count+=1 ; 
+            if i >= 2  and arr[i-1][j] == "E" and arr[i-2][j] == "E" : count+=1; 
+            if i >= 2 and j < n-3 and arr[i-1][j+1] == "E" and arr[i-2][j+2] == "E" : count+=1;
+
+
+
+
+
+
+
+
+            
+
+
+print(count)
