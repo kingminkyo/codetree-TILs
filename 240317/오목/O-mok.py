6b36ba8b260529a4d325211a83fbdf68b28936a7
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-17 16:55:14 +0900

    Title: 오목, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240317/오목/O-mok.py
+++ 240317/오목/O-mok.py
@@ -50,7 +50,7 @@ for i in range(19):
                 elif win == 12 :
                     x1, x2 = i+3, j+1 
                 else:
-                    x1, x2 = i+3, j+1
+                    x1, x2 = i+3, j-1
                 break
             
         if win >= 1:
