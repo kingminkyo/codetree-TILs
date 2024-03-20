87955dc740e6574f598df87e09a5c91fae4d2d32
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-20 18:13:25 +0900

    Title: 야바위, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240320/야바위/ya-rock.py
+++ 240320/야바위/ya-rock.py
@@ -10,17 +10,17 @@ for i in range(3):
     abc[i] = 1
     score = 0
     for j in range(n):
-        # print(abc)
+        
         temp = abc[arr[j][0]-1]
         abc[arr[j][0]-1] = abc[arr[j][1]-1]
         abc[arr[j][1]-1] = temp
 
         if abc[arr[j][2]-1] == 1:
             score+=1
-
+        # print(abc)
         # print(score)
 
     result = max(score, result)
-    abc[i] = 0
+    abc[0], abc[1], abc[2] = 0
 
 print(result)
