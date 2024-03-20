25cb1947370ded9b29b25bad1ab7b6c7d1a182da
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-21 03:22:07 +0900

    Title: 마을 구분하기, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240321/마을 구분하기/seperate-village.py
+++ 240321/마을 구분하기/seperate-village.py
@@ -38,9 +38,9 @@ comp = 0
 for i in range(n):
     for j in range(n):
 
-        if can_go(i, j):
-            count= 0
-            visited[i][j] = 1
+        # if can_go(i, j):
+        #     count= 1
+        #     visited[i][j] = 1
             dfs(i, j)
             if comp is not count:
                 result.append(count)
