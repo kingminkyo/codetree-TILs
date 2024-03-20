846b11a7290d15a02c8c5237bac522bdb5631d6b
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-21 00:57:37 +0900

    Title: 그래프 탐색, Time: 170ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240321/그래프 탐색/graph-traversal.py
@@ -0,0 +1,26 @@
+n, m = tuple(map(int, input().split()))
+
+graph = [[] for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+vertex_cnt = 0
+
+for i in range(m):
+    v1, v2 = tuple(map(int, input().split()))
+
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+
+def dfs(vertex):
+    global vertex_cnt
+    for curr_v in graph[vertex]:
+        if not visited[curr_v]:
+            # print(curr_v)
+            vertex_cnt+=1
+            visited[curr_v] = True
+            dfs(curr_v)
+
+visited[1] = True
+dfs(1)
+
+print(vertex_cnt)
