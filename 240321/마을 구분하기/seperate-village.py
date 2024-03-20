1dbfb53171afad3ad41cba6ca20de068dc455f3f
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-21 02:55:20 +0900

    Title: 마을 구분하기, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240321/마을 구분하기/seperate-village.py
@@ -0,0 +1,51 @@
+n = int(input())
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0 for _ in range(n) ]
+    for _ in range(n)
+]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    
+    if arr[x][y] == 0 or visited[x][y]:
+        return False
+
+    return True
+
+count = 0
+def dfs(x, y):
+    dxs, dys = [1, 0, -1, 0] , [0, -1, 0, 1]
+    global count
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        if can_go(new_x, new_y):
+            count += 1 
+            visited[new_x][new_y] = 1
+            dfs(new_x, new_y)
+comp = 0
+result = []
+for i in range(n):
+    for j in range(n):
+        dfs(i, j)
+        if comp is not count:
+            result.append(count)
+            count = 0
+        
+
+# for i in range(n):
+#     print(visited[i])
+
+print(len(result))
+result.sort()
+for i in range(len(result)):
+    print(result[i])
