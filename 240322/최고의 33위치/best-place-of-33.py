5377ff73f7220b8a0ae4e1342d1b2bf4a69ea838
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-22 16:13:46 +0900

    Title: 최고의 33위치, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+result = 0
+
+def count_area(x, y):
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if grid[i][j] == 1:
+                count += 1
+
+    return count
+
+
+
+for i in range(n-2):
+    for j in range(n-2):
+        result = max(result, count_area(i, j))
+
+print(result)
