aab322569f9e14f5dfff96f37c62488bb8a28b70
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-22 14:30:26 +0900

    Title: 아름다운 수, Time: 325ms, Memory: 28MB, Status: Passed - CodeTree

--- 240322/아름다운 수/beautiful-number.py
+++ 240322/아름다운 수/beautiful-number.py
@@ -42,18 +42,35 @@ def check_double():
         double_stack = []
         return False
 
-def is_beautiful():
-    i = 0
-    for i in range(0, n, result[i]):
-        if result[i] > n:
-            return False
-        for j in range(i, i + result[i]):
-            if result[i] != result[j]:
-                return False
+# def is_beautiful():
+#     i = 0
+#     for i in range(0, n, result[i]):
+#         if result[i] > n:
+#             return False
+#         for j in range(i, i + result[i]):
+#             if result[i] != result[j]:
+#                 return False
         
+        
+#     return True
+def is_beautiful():
+    # 연달아 같은 숫자가 나오는 시작 위치를 잡습니다.
+    i = 0
+    while i < n:
+        # 만약 연속하여 해당 숫자만큼 나올 수 없다면
+        # 아름다운 수가 아닙니다.
+        if i + result[i] - 1 >= n:
+            return False
+        # 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인합니다.
+        # 하나라도 다른 숫자가 있다면
+        # 아름다운 수가 아닙니다.
+        for j in range(i, i + result[i]):
+            if result[j] != result[i]:
+                return False
+            
+        i += result[i]
         
     return True
-
 
 end = 0
 def find_bt(cnt):
