13e5c9041a8d539f0ccb79bc801dd12b2dfcf8ef
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-03-22 13:20:52 +0900

    Title: 아름다운 수, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/아름다운 수/beautiful-number.py
@@ -0,0 +1,62 @@
+n = int(input())
+# n = 3
+result = []
+
+def print_bt():
+    for i in result:
+        print(i, end="")
+
+    print()
+
+double_stack = []
+# print(len(double_stack))
+
+def check_double():
+    global double_stack
+    for i in range(n): # 0부터 탐색
+
+        # 2 4 2 를 가정하자 
+        if len(double_stack) == 0 and result[i] >= 2 :
+            for j in range(result[i]-1):
+                double_stack.append(result[i])
+
+
+        elif result[i] >= 2 and  double_stack[len(double_stack)-1] != result[i]:
+            for j in range(result[i]-1):
+                double_stack.append(result[i])
+        
+        elif len(double_stack) != 0 and result[i-1] == result[i]:
+            double_stack.pop()
+        
+        
+    # print(double_stack)
+
+    if len(double_stack) == 0:
+        double_stack = []
+        return True
+    else:
+        double_stack = []
+        return False
+
+
+
+
+end = 0
+def find_bt(cnt):
+    global end
+    if cnt == n+1:
+        # print_bt()
+        
+        if check_double():
+            end+=1
+        return
+
+    for i in range(1, 5):
+        result.append(i)
+
+        find_bt(cnt+1)
+        result.pop()
+
+find_bt(1)
+
+print(end)
