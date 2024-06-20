b9d18bd764b2dbc1f4268372f607d1df9174ceee
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-20 16:52:50 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240620/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,23 @@
+direct = input()
+
+x, y = 0, 0
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+
+c_dir = 3
+
+for d in direct:
+
+    if d == "F":
+        x += dx[c_dir]
+        y += dy[c_dir]
+
+    if d == "L":
+        c_dir -= 1
+    if d == "R":
+        c_dir += 1
+
+    if c_dir >= 4:
+        d = 0
+
+    
+print(f"{x} {y}")
