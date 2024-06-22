1b3a1378168d8f2ceee8a48a05eededf2afadab9
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-22 19:05:53 +0900

    Title: 격자 위의 편안한 상태, Time: 151ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240622/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,26 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [
+    [0]*n for _ in range(n)
+]
+
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+    arr[x-1][y-1] = 1
+
+    cnt = 0
+
+    for dxs, dys in zip(dx, dy):
+        nx, ny  = x-1+dxs, y-1+dys
+        if in_range(nx, ny) and arr[nx][ny] ==1:
+            cnt+= 1
+
+    if cnt>=3:
+        print(1)
+    else:
+        print(0)
