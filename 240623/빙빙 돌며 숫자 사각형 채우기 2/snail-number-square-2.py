f610b35a795c2fd119afd82efd59fdf62d11cfa8
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-23 19:10:10 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240623/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 240623/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -9,18 +9,18 @@ arr[0][0] = 1
 dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1]
 
 def in_range(x, y):
-    return x>=0 and x<m and y>=0 and y<n
+    return x>=0 and x<n and y>=0 and y<m
 
 d_dir = 0
 x, y = 0, 0
 
 for i in range(2, m*n+1):
     x, y = x+dxs[d_dir], y+dys[d_dir]
-    arr[x][y] = i
 
     if not in_range(x+dxs[d_dir], y+dys[d_dir]) or arr[x+dxs[d_dir]][y+dys[d_dir]] != 0:
         d_dir = (d_dir + 1) % 4
 
+    arr[x][y] = i
 
 for i in range(n):
     for j in range(m):
