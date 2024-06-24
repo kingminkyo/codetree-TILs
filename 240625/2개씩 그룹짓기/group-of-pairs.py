d2fe5aaae21ffb317c8e306546c3baeee836e77e
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-25 02:00:02 +0900

    Title: 2개씩 그룹짓기, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240625/2개씩 그룹짓기/group-of-pairs.py
+++ 240625/2개씩 그룹짓기/group-of-pairs.py
@@ -1,34 +1,13 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-arr.sort(reverse = True)
+arr.sort()
 
-i = 2
-a1 = []
-a2 = []
+g_max = 0
 
-for a in arr:
-    if i == 0:
-        a1.append(a)
-    elif i  == 1:
-        a2.append(a)
-    elif i  == 2:
-        a2.append(a)
-    else:
-        a1.append(a)
-    i = (i + 1) % 4
+for i in range(n):
+    g_sum = arr[i] + arr[2*n -1 -i]
+    if g_max < g_sum:
+        g_max = g_sum
 
-a1sum = 0
-a2sum = 0
-
-for (a, b) in zip(a1, a2):
-    a1sum += a
-    a2sum += b
-
-result = 0
-
-if  a1sum > a2sum:
-    result = a1sum
-else:
-    result = a2sum
-print(result)
+print(g_max)
