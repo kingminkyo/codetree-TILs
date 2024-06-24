3c2e76d456d9c3ec28b3a6eed6d0c1a2161849fe
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-25 08:49:02 +0900

    Title: 중앙값 계산 2, Time: 119ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240625/중앙값 계산 2/get-median-2.py
+++ 240625/중앙값 계산 2/get-median-2.py
@@ -4,8 +4,9 @@ arr = list(map(int, input().split()))
 words = []
 for a in arr:
     words.append(a)
-    mid = int(len(words)/2)
+    mid = int(len(words)/2) -1
     words.sort()
 
     if a % 2 == 1:
+        # print(words)
         print(words[mid], end=" ")
