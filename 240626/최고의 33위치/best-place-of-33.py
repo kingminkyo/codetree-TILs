4816a380e7c9ed0dd5c6b069cc8d7b7a98d29e8f
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-26 08:21:02 +0900

    Title: 최고의 33위치, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240626/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+def in_range(x, y):
+    return x>= 0 and x<n and y>=0 and y<n
+
+result = 0
+count = 0
+for i in range(n-2):
+    for j in range(n-2):
+        for a in range(3):
+            for b in range(3):
+                if in_range(i+a, i+b):
+                    count = count + arr[i+a][i+b]       
+        
+        if count > result:
+            result = count
+        
+        count = 0
+
+print(result)
