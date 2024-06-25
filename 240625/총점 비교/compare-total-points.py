0fbc0fa595543d93352f8d02a144088b14b49ab2
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-25 09:53:55 +0900

    Title: 총점 비교, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/총점 비교/compare-total-points.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, n, a, b, c):
+        self.n, self.a, self.b, self.c = n, a, b, c
+        self.total = a+b+c
+
+n = int(input())
+
+students = []
+for _ in range(n):
+    n, a, b, c = tuple(input().split())
+    students.append(Student(n, a, b, c))
+
+students.sort(key = lambda x:x.total)
+
+for s in students:
+    print(f"{s.n} {s.a} {s.b} {s.c}", end=" ")
