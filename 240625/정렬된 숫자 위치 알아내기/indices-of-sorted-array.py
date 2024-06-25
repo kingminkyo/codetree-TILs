c32bd4e9c40bbcdd5a9739e7aab537970d898f21
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-25 12:07:04 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240625/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 240625/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -12,7 +12,7 @@ nums = []
 for a in arr:
     nums.append(Num(a, i))
     i+=1 
-
+i = 1
 for a in arr:
     orin.append(Num(a, i))
     i+=1 
@@ -21,7 +21,6 @@ nums.sort(key = lambda x:x.n)
 
 for a in orin:
     for ii, n in enumerate(nums, start=1):
-        print(a.i)
+        # print(a.i)
         if a.i == n.i:
-    
             print(f"{ii}", end=" ")
