f6639aec929b90ce7d4bf7c58355e26d7a82b044
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-25 10:27:14 +0900

    Title: 개인정보, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240625/개인정보/personal-info.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, n, h, w):
+        self.n, self.h, self.w = n, h, w
+
+n = int(input())
+persons = []
+for _ in range(n):
+    n, h, w = tuple(input().split())
+    h, w = int(h), float(w)
+
+    persons.append(Person(n, h, w))
+
+print("name")
+persons.sort(key = lambda x:x.n)
+
+for p in persons:
+    print(f"{p.n} {p.h} {p.w}")
+print()
+
+print("height")
+persons.sort(key = lambda x:x.h)
+
+for p in persons:
+    print(f"{p.n} {p.h} {p.w}")
