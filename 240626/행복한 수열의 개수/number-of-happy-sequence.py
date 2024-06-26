ce1a1ab6b1a9248b56436a299ea65fd6c32578c4
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-26 14:10:43 +0900

    Title: 행복한 수열의 개수, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240626/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240626/행복한 수열의 개수/number-of-happy-sequence.py
@@ -15,16 +15,21 @@ for i in range(n):
     for j in range(n):
         if j>0 and arr[i][j] == arr[i][j-1]:
             cnt += 1
+        else:
+            cnt = 0
             # print(i, j)
         if cnt == m:
             result += 1 
             break
+            
 cnt = 0
 for i in range(n):
     cnt = 0
     for j in range(n):
         if j>0 and arr[j][i] == arr[j-1][i]:
             cnt += 1
+        else:
+            cnt = 0 
             
         if cnt == m:
             result += 1 
