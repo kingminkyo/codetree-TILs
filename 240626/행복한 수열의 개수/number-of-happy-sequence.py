40175ebfb5df58a58c08bf5f24170c0f7deeec8a
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-26 11:50:24 +0900

    Title: 행복한 수열의 개수, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240626/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240626/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,28 +5,28 @@ arr = [
     for _ in range(n)
 ]
 
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
 result = 0
 
 for i in range(n):
-    count = 0
-    for j in range(1, n):
-        if arr[i][j] == arr[i][j-1]:
-            count+= 1 
-            # print(i, j, count)
-
-        if count >= m:
+    cnt = 0
+    for j in range(n):
+        if in_range(i, j-1) and arr[i][j] == arr[i][j-1]:
+            cnt += 1
+            
+        if cnt == m:
             result += 1 
-            break
 
 for i in range(n):
-    count = 0
-    for j in range(1, n):
-        if arr[j][i] == arr[j-1][i]:
-            count+= 1 
-            # print(i, j, count)
-
-        if count >= m:
+    cnt = 0
+    for j in range(n):
+        if in_range(j-1, i) and arr[j][i] == arr[j-1][i]:
+            cnt += 1
+            
+        if cnt == m:
             result += 1 
-            break
+
 
 print(result)
