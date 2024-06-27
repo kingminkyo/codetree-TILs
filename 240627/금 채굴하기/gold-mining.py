2421387433441750bfb5e44f7aae45a14c82b90d
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-27 23:21:44 +0900

    Title: 금 채굴하기, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240627/금 채굴하기/gold-mining.py
@@ -0,0 +1,32 @@
+n, m = tuple(map(int, input().split()))
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+
+k = n-1 
+
+def get_area(k):
+    return k*k + (k+1 ) * (k+1)
+
+def get_num_of_gold(x, y, k):
+    result = 0
+    for i in range(n):
+        for j in range(n):
+            if abs(x-i) + abs(y-j) <= k:
+                result += arr[i][j]
+
+    return result
+
+max_gold = 0
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            nog = get_num_of_gold(i, j, k)
+
+            if nog * m >= get_area(k):
+                max_gold = max(max_gold, nog)
+
+print(max_gold)
