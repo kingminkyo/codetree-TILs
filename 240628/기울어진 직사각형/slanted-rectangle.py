928e07861a965734d17bb996bfe7fe5ecb5aedfc
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-28 00:09:59 +0900

    Title: 기울어진 직사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240628/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,41 @@
+n = int(input())
+arr ={
+    list(map(int, input().split()))
+    for _ in range(n)
+}
+
+dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1] 
+
+
+def in_range(x, y):
+    return x>=0 and x< n and y>=0 and y<n
+
+def square(x, y, k, l):
+    nx, ny = x, y 
+    result = 0
+    
+    move_nums = [k, l, k, l]
+
+    for dx, dy, move_num in zip(dxs, dys, move_num):
+        for _ in range(move_num):
+            x, y = x+dx, y+dy
+
+            if not in_range(x, y):
+                return 0
+        
+            result += arr[x][y]
+        
+    return result
+
+
+ans = 0
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            for l in range(n):
+                ans = max(ans, square(i, j, k, l))
+
+
+    
+print(ans)
