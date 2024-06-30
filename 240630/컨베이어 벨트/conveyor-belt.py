5f14d0edb61a0f37ec23ae76c561bc0c86c472e0
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-06-30 17:09:39 +0900

    Title: 컨베이어 벨트, Time: 111ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240630/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,25 @@
+n, t = map(int, input().split())
+
+
+u = list(map(int, input().split()))
+d = list(map(int, input().split()))
+
+
+for _ in range(t):
+    
+    temp = u[n-1]
+
+    for i in range(n-1, 0, -1):
+        u[i] = u[i-1]
+    u[0] = d[n-1]
+
+    for i in range(n-1, 0, -1):
+        d[i] = d[i-1]
+    d[0] = temp
+
+
+for a in u:
+    print(a, end=" ")
+print()
+for b in d:
+    print(b, end=" ")
