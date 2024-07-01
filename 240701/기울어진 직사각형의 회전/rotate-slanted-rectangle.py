e7e6b4e701bd9a76134778af37a1def404ccb234
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-01 12:58:24 +0900

    Title: 기울어진 직사각형의 회전, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240701/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240701/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -20,10 +20,11 @@ r, c, k, l, k, l, d = map(int, input().split())
 
 r, c = r-1, c-1 
 
-dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
 
 
 def clockwise(r, c, k, l):
+
+    dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
     
     for i in range(1, k+1):
         nx, ny = r+dxs[0]*i, c+dys[0]*i
@@ -51,7 +52,40 @@ def clockwise(r, c, k, l):
         ex, ey = r+dxs[3]*(i-1), c+dys[3]*(i-1)
         arr[nx][ny] = sub[ex][ey]
     
-clockwise(r, c, k, l)
+def clock(r, c, k, l):
+
+    dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
+    
+    for i in range(1, k+1):
+        nx, ny = r+dxs[0]*i, c+dys[0]*i
+        ex, ey = r+dxs[0]*(i-1), c+dys[0]*(i-1)
+        arr[nx][ny] = sub[ex][ey]
+
+    r, c = r+dxs[0]*k, c+dys[0]*k
+
+    for i in range(1, l+1):
+        nx, ny = r+dxs[1]*i, c+dys[1]*i
+        ex, ey = r+dxs[1]*(i-1), c+dys[1]*(i-1)
+        arr[nx][ny] = sub[ex][ey]
+
+    r, c = r+dxs[1]*l, c+dys[1]*l
+
+    for i in range(1, k+1):
+        nx, ny = r+dxs[2]*i, c+dys[2]*i
+        ex, ey = r+dxs[2]*(i-1), c+dys[2]*(i-1)
+        arr[nx][ny] = sub[ex][ey]
+
+    r, c = r+dxs[2]*k, c+dys[2]*k
+
+    for i in range(1, l+1):
+        nx, ny = r+dxs[3]*i, c+dys[3]*i
+        ex, ey = r+dxs[3]*(i-1), c+dys[3]*(i-1)
+        arr[nx][ny] = sub[ex][ey]
+
+if d == 0:
+    clockwise(r, c, k, l)
+else:
+    clock(r, c, k, l)
 
 for i in range(n):
     for j in range(n):
