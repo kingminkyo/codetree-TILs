3137608cdc25cd8be7e39d33736495ca0c195f5e
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-01 13:07:00 +0900

    Title: 기울어진 직사각형의 회전, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240701/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240701/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -15,77 +15,77 @@ for i in range(n):
 
 
 
-r, c, k, l, k, l, d = map(int, input().split())
+r, c, m1, m2, m3, m4, d = map(int, input().split())
 
 
 r, c = r-1, c-1 
 
 
 
-def clockwise(r, c, k, l):
+def clockwise(r, c, m1, m2, m3, m4):
 
     dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
     
-    for i in range(1, k+1):
+    for i in range(1, m1+1):
         nx, ny = r+dxs[0]*i, c+dys[0]*i
         ex, ey = r+dxs[0]*(i-1), c+dys[0]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[0]*k, c+dys[0]*k
+    r, c = r+dxs[0]*m1, c+dys[0]*m1
 
-    for i in range(1, l+1):
+    for i in range(1, m2+1):
         nx, ny = r+dxs[1]*i, c+dys[1]*i
         ex, ey = r+dxs[1]*(i-1), c+dys[1]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[1]*l, c+dys[1]*l
+    r, c = r+dxs[1]*m2, c+dys[1]*m2
 
-    for i in range(1, k+1):
+    for i in range(1, m3+1):
         nx, ny = r+dxs[2]*i, c+dys[2]*i
         ex, ey = r+dxs[2]*(i-1), c+dys[2]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[2]*k, c+dys[2]*k
+    r, c = r+dxs[2]*m3, c+dys[2]*m3
 
-    for i in range(1, l+1):
+    for i in range(1, m4+1):
         nx, ny = r+dxs[3]*i, c+dys[3]*i
         ex, ey = r+dxs[3]*(i-1), c+dys[3]*(i-1)
         arr[nx][ny] = sub[ex][ey]
     
-def clock(r, c, k, l):
+def clock(r, c, m1, m2, m3, m4):
 
     dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
     
-    for i in range(1, k+1):
+    for i in range(1, m1+1):
         nx, ny = r+dxs[0]*i, c+dys[0]*i
         ex, ey = r+dxs[0]*(i-1), c+dys[0]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[0]*k, c+dys[0]*k
+    r, c = r+dxs[0]*m1, c+dys[0]*m1
 
-    for i in range(1, l+1):
+    for i in range(1, m2+1):
         nx, ny = r+dxs[1]*i, c+dys[1]*i
         ex, ey = r+dxs[1]*(i-1), c+dys[1]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[1]*l, c+dys[1]*l
+    r, c = r+dxs[1]*m2, c+dys[1]*m2
 
-    for i in range(1, k+1):
+    for i in range(1, m3+1):
         nx, ny = r+dxs[2]*i, c+dys[2]*i
         ex, ey = r+dxs[2]*(i-1), c+dys[2]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[2]*k, c+dys[2]*k
+    r, c = r+dxs[2]*m3, c+dys[2]*m3
 
-    for i in range(1, l+1):
+    for i in range(1, m4+1):
         nx, ny = r+dxs[3]*i, c+dys[3]*i
         ex, ey = r+dxs[3]*(i-1), c+dys[3]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
 if d == 0:
-    clockwise(r, c, k, l)
+    clockwise(r, c, m1, m2, m3, m4)
 else:
-    clock(r, c, k, l)
+    clock(r, c, m1, m2, m3, m4)
 
 for i in range(n):
     for j in range(n):
