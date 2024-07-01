a4dc8bf1dbe782657a7624490db62b3a76785810
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-01 13:15:10 +0900

    Title: 기울어진 직사각형의 회전, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240701/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240701/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -56,28 +56,28 @@ def clock(r, c, m1, m2, m3, m4):
 
     dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
     
-    for i in range(1, m1+1):
+    for i in range(1, m2+1):
         nx, ny = r+dxs[0]*i, c+dys[0]*i
         ex, ey = r+dxs[0]*(i-1), c+dys[0]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[0]*m1, c+dys[0]*m1
+    r, c = r+dxs[0]*m2, c+dys[0]*m2
 
-    for i in range(1, m2+1):
+    for i in range(1, m1+1):
         nx, ny = r+dxs[1]*i, c+dys[1]*i
         ex, ey = r+dxs[1]*(i-1), c+dys[1]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[1]*m2, c+dys[1]*m2
+    r, c = r+dxs[1]*m1, c+dys[1]*m1
 
-    for i in range(1, m3+1):
+    for i in range(1, m2+1):
         nx, ny = r+dxs[2]*i, c+dys[2]*i
         ex, ey = r+dxs[2]*(i-1), c+dys[2]*(i-1)
         arr[nx][ny] = sub[ex][ey]
 
-    r, c = r+dxs[2]*m3, c+dys[2]*m3
+    r, c = r+dxs[2]*m2, c+dys[2]*m2
 
-    for i in range(1, m4+1):
+    for i in range(1, m1+1):
         nx, ny = r+dxs[3]*i, c+dys[3]*i
         ex, ey = r+dxs[3]*(i-1), c+dys[3]*(i-1)
         arr[nx][ny] = sub[ex][ey]
