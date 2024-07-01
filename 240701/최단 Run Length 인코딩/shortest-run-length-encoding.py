34b5156f978fb7d74df89247e4bbea1ead7489d9
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-01 13:52:11 +0900

    Title: 최단 Run Length 인코딩, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240701/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240701/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,20 +1,37 @@
 word = input()
 
-
-
-
-
 def incoding(word):
 
     new_word = []
-    cnt = 0
-    for i in range(1, len(word)-1):
+    cnt = 1
+    for i in range(1, len(word)):
         if word[i] == word[i-1]:
             cnt += 1 
         else:
             new_word.append(word[i-1])
-            new_word.append(chr(cnt))
+            new_word.append(cnt)
+            cnt = 1
+        
+        if i == (len(word)-1):
+            new_word.append(word[i-1])
+            new_word.append(cnt)
+
+    if new_word[1] == 10:
+        return 3
 
     return len(new_word)
 
-print(incoding(word))
+
+min_result = 10000
+word = list(word)
+
+for _ in range(len(word)):
+    temp = word[len(word)-1]
+
+    for i in range(len(word)-1, 0, -1):
+        word[i] = word[i-1]
+    word[0] = temp
+
+    min_result = min(incoding(word), min_result)
+
+print(min_result)
