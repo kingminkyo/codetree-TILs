696bed1e88486ab91325d5fc14890816248c4fe2
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-03 18:22:02 +0900

    Title: 뱀은 사과를 좋아해, Time: 169ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240703/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 240703/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -1,37 +1,39 @@
-n, m, k = tuple(map(int, input().split()))
+n, m, k = map(int, input().split())
 
 class Snake:
     def __init__(self, x, y, num):
-        self.x, self.y , self.num = int(x), int(y), int(num )
-        
+        self.x = x
+        self.y = y
+        self.num = num
+
 class Command:
     def __init__(self, dct, num):
         self.dct = dct
         self.num = num 
 
-arr = [
-    [0 for _ in range(n)]
-    for _ in range(n)
-]
+# 격자 초기화
+arr = [[0 for _ in range(n)] for _ in range(n)]
 
+# 사과 위치 입력 받기
 for _ in range(m):
     x, y = map(int, input().split())
-    x, y = x-1, y-1 
+    x, y = x-1, y-1  # 0부터 시작하는 인덱스로 변환
     arr[x][y] = 1 
 
+# 명령 입력 받기
 commands = []
-
-
-
 for _ in range(k):
     d, a = input().split()
     commands.append(Command(d, int(a)))
 
+# 범위 체크 함수
 def in_range(x, y):
-    return x>=0 and x<n and y>=0 and y<n
+    return 0 <= x < n and 0 <= y < n
 
+# 뱀의 몸통을 저장할 리스트
 roots = []
 
+# 디버깅을 위한 함수 (뱀의 현재 상태 출력)
 def print_roots():
     print("root: ", end=" ")
     for root in roots:
@@ -39,16 +41,19 @@ def print_roots():
     print()
     print()
 
-
+# 방향 설정 (우, 하, 좌, 상)
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 d_dir = 0
 
+# 초기 위치 설정
 x, y = 0, 0 
 cnt = 0
-# print(type(cnt))
-def root_sort():
-    roots.sort(key= lambda x: -x.num)
 
+# 뱀의 몸통을 정렬하는 함수 (num 기준 내림차순)
+def root_sort():
+    roots.sort(key=lambda s: -s.num)
+
+# 명령 수행
 for c in commands:
     
     if c.dct == "R":
@@ -62,51 +67,33 @@ for c in commands:
 
     is_gameover = False 
 
-    for i in range(c.num):
-        # print(cnt)
-        nx, ny = x+dxs[d_dir], y+dys[d_dir]
-        # print(c.dct, i)
+    for _ in range(c.num):
+        nx, ny = x + dxs[d_dir], y + dys[d_dir]
         if not in_range(nx, ny):
-            # print("아웃")
-            cnt+= 1
+            cnt += 1
             is_gameover = True
             break
 
         # 사과 발견 시 
         if arr[nx][ny] == 1:
-            
             roots.append(Snake(x, y, cnt))
             root_sort()
             x, y = nx, ny
             arr[nx][ny] = 0
             cnt += 1 
-            # print("eat", x, y)
-            # print_roots()
-            
-        else : 
+        else:
             if len(roots) != 0:
                 roots.pop()
                 roots.append(Snake(x, y, cnt))
                 root_sort()
             x, y = nx, ny
-            # if cnt>=28:
-            #     print(x, y)
             cnt += 1 
-            # print_roots()
 
-        #꼬리랑 겹치는 경로인지 탐색 
+        # 뱀의 몸통과 부딪혔는지 체크
         for root in roots:
-            # if cnt>=33:
-            #     print()
-            #     print("체크")
-            #     print(x, y)
-            #     print(f"{root.x} {root.y}")
             if x == root.x and y == root.y:
-                # print("게임 오버")
                 is_gameover = True
                 break
-            # else:
-            #     print(f"아니지롱 {root.x} {root.y}")
 
     if is_gameover:
         break        
