5f3597f305dfdb700a6a18cb0cbc7630a009120b
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-04 10:07:03 +0900

    Title: 숫자의 순차적 이동, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/숫자의 순차적 이동/sequential-movement-of-numbers.py
@@ -0,0 +1,44 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+dxs, dys = [0, 1, 1, 1, 0, -1, -1, -1], [1, 1, 0, -1, -1, -1, 0, 1]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+
+def swap(x, y):
+    num = arr[x][y]
+    cx, cy = x, y
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+        if in_range(nx, ny) and arr[nx][ny] > num:
+            cx, cy = nx, ny
+            num = arr[nx][ny]
+    
+    arr[x][y], arr[cx][cy] = arr[cx][cy], arr[x][y]
+            
+
+    
+
+for _ in range(m):
+    for num in range(1, n*n+1):
+        max_num = -1 
+        x, y = 0, 0 
+        ok = False
+        for i in range (n):
+            for j in range(n):
+                if arr[i][j] == num:
+                    swap(i, j)
+                    ok = True
+                    break
+            if ok:
+                break
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end=" ")
+    print()
