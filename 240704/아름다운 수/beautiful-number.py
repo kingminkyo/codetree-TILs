0dbfd53a521fa03dc97e98ef6605df5a39aaa48a
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-04 19:58:00 +0900

    Title: 아름다운 수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/아름다운 수/beautiful-number.py
@@ -0,0 +1,49 @@
+n = int(input())
+
+ans = []
+
+cnt = 0
+
+def print_answer():
+    global cnt
+    for a in ans:
+        print(a, end="")
+    print(f" {cnt}" ,end="")
+    print()
+
+
+
+def check_beautiful():
+    global cnt 
+    check = []
+
+    for a in ans:
+        check.append(a)
+        if len(check) > 0:
+            for c in check:
+                if a != c:
+                    return 0
+            
+            if check[0] == len(check):
+                for _ in range(len(check)):
+                    check.pop()
+        print(check)
+    if len(check) == 0:
+        cnt += 1 
+    
+
+
+def choose(curr_num):
+
+    if curr_num == n+1:
+        # print_answer()
+        check_beautiful()
+        return
+    
+    for i in range(1, 5):
+        ans.append(i)
+        choose(curr_num + 1)
+        ans.pop()
+
+
+choose(1)
