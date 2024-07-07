a162326f53a4eb9485e6e211ae9da85d5822116a
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-08 07:20:37 +0900

    Title: 그래프 탐색, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240708/그래프 탐색/graph-traversal.py
@@ -0,0 +1,33 @@
+n, m = tuple(map(int, input().split()))
+
+
+arr = [
+    [0] for _ in range(n+1)
+]
+visited = [ False for _ in range(n+1)]
+
+# print(arr)
+cnt = 0
+def dfs(x):
+    global cnt
+
+    for curr_num in arr[x]:
+
+        if not visited[curr_num]:
+            cnt += 1 
+            visited[curr_num] = True
+            dfs(curr_num)
+
+
+
+
+visited[1] = True
+
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+
+    arr[x].append(y)
+    arr[y].append(x)
+
+dfs(1)
+print(cnt)
