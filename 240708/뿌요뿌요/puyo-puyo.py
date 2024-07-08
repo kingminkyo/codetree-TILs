b046053e09d2f4a876678da4f20bad8a410e3c6e
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-08 12:29:09 +0900

    Title: 뿌요뿌요, Time: 153ms, Memory: 73MB, Status: Wrong Answer - CodeTree

---
+++ 240708/뿌요뿌요/puyo-puyo.py
@@ -0,0 +1,53 @@
+n = int(input())
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+def in_range(x, y ):
+    return x>=0 and x<n and y>=0 and y<n
+
+def can_go(x, y, num):
+    if not in_range(x, y):
+        return False
+    
+    if visited[x][y] or arr[x][y] != num:
+        return False
+    return True
+
+block_cnt = 0
+block_size = 0
+
+def dfs(x, y):
+    global block
+
+    num = arr[x][y]
+
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+
+        if can_go(nx, ny, num):
+            block += 1 
+            visited[nx][ny] = 1 
+            dfs(nx, ny)
+
+
+
+for i in range(n):
+    for j in range(n):
+        block = 0
+        dfs(i, j)
+
+        if block > 0:
+            block_cnt += 1 
+            block_size = max(block_size, block)
+        
+print(block_cnt, block_size)
