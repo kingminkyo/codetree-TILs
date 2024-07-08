b782098b1fd9e07db5fa6e5139e21289e3e494dd
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-08 11:51:43 +0900

    Title: 안전 지대, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240708/안전 지대/comfort-zone.py
@@ -0,0 +1,65 @@
+n, m = tuple(map(int, input().split()))
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<m
+
+def can_go(x,y, k):
+    if not in_range(x, y):
+        return False
+
+    if visited[x][y] or arr[x][y] > k :
+        return False
+    return True
+
+max_k = 0
+
+for i in range(n):
+    for j in range(m):
+        max_k = max(max_k, arr[i][j])
+
+def dfs(x, y, k):
+
+    dxs, dys = [0, 0, 1, 0, -1], [0, 1, 0, -1, 0]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+
+        if can_go(nx, ny, k):
+
+            visited[nx][ny] = 1
+            dfs(nx, ny, k)
+
+def reset_all():
+    visited = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+    ]
+
+
+cnt = 0
+result_k = 0
+result_a = 0
+for k in range(1, max_k+1):
+    cnt = 0
+    reset_all()
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] <= k and visited[i][j] == 0:
+                cnt += 1 
+                dfs(i, j, k)
+
+    if result_a < cnt:
+        result_a = cnt
+        result_k = k
+
+
+print(result_k-1, result_a)
