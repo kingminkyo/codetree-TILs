d2ccaa3bdd2a189a0bd32fc48dc137ad70412f1f
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-10 14:44:25 +0900

    Title: 최소 경로로 탈출 하기, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240710/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,45 @@
+n, m = tuple(map(int, input().split()))
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0 for i in range(n)]
+    for _ in range(n)
+]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] or arr[x][y] == 0:
+        return False
+    return True
+
+from collections import deque
+
+q = deque()
+
+def push(x, y, num):
+    visited[x][y] = num
+    q.append((x, y))
+
+def bfs():
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+
+            if can_go(nx, ny):
+                push(nx, ny, visited[x][y] + 1)
+
+push(0, 0, 0)
+bfs()
+
+print(visited[n-1][m-1])
