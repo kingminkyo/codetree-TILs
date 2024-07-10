8c2192365162e217d2eca2fcc25f4fb04dcadff7
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-10 14:55:09 +0900

    Title: 나이트, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240710/나이트/knight-movements.py
@@ -0,0 +1,44 @@
+n = int(input())
+r1, c1, r2, c2 = tuple(map(int, input().split()))
+r1, c1, r2, c2 = r1-1, c1-1, r2-1, c2-1 
+visited = [
+    [0 for i in range(n)]
+    for _ in range(n)
+]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y]:
+        return False
+    return True
+    
+
+from collections import deque
+q = deque()
+
+def push(x, y, num):
+    visited[x][y] = num
+    q.append((x, y))
+def bfs():
+
+    dxs, dys = [-2, -1, 1, 2, 2, 1, -1, -2], [1, 2, 2, 1, -1, -2, -2, -1]
+
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if can_go(nx, ny):
+                push(nx, ny, visited[x][y] + 1)
+
+
+push(r1, c1, 0)
+bfs()
+
+if visited[r2][c2]:
+    print( visited[r2][c2])
+else:
+    print(-1)
