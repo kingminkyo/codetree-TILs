d1af0d1a7f388c5c161e5274a4081aa1794ed057
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-11 16:04:36 +0900

    Title: 상한 귤, Time: 5074ms, Memory: 226MB, Status: Time Limit Exceed - CodeTree

---
+++ 240711/상한 귤/oranges-have-gone-bad.py
@@ -0,0 +1,52 @@
+n, k = tuple(map(int, input().split()))
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [-1 for _ in range(n)]
+    for _ in range(n)
+]
+
+def print_visited():
+    for i in range(n):
+        for j in range(n):
+            print(visited[i][j], end=" ")
+        print()
+    print()
+     
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+def can_go(x, y, num):
+    if not in_range(x, y):
+        return False
+    if arr[x][y] == 0 or (visited[x][y] <= num and visited[x][y] != -1):
+        return False
+    return True
+
+from collections import deque
+q = deque()
+
+def push(x, y, num):
+    q.append((x, y))
+    visited[x][y] = num
+
+def bfs():
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if can_go(nx, ny, visited[x][y]):
+                push(nx, ny, visited[x][y] + 1)
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 2:
+            push(i, j, 0)
+            bfs()
+
+print_visited()
