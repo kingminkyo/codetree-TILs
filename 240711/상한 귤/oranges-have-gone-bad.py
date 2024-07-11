b5f8e661eedc38e55156aa81db07deacd52c69ca
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-11 16:16:10 +0900

    Title: 상한 귤, Time: 3070ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240711/상한 귤/oranges-have-gone-bad.py
+++ 240711/상한 귤/oranges-have-gone-bad.py
@@ -1,43 +1,58 @@
+n, k = tuple(map(int, input().split()))
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [-1 for _ in range(n)]
+    for _ in range(n)
+]
+
+def print_visited():
+    for i in range(n):
+        for j in range(n):
+            print(visited[i][j], end=" ")
+        print()
+    print()
+     
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+def can_go(x, y, num):
+    if not in_range(x, y):
+        return False
+    if arr[x][y] == 0 or (visited[x][y] <= num and visited[x][y] != -1):
+        return False
+    return True
+
 from collections import deque
-
-# 입력 받기
-n, k = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-# 방문 시간을 기록할 배열
-visited = [[-1 for _ in range(n)] for _ in range(n)]
-
-# 방향 벡터 (우, 하, 좌, 상)
-dxs = [0, 1, 0, -1]
-dys = [1, 0, -1, 0]
-
-# 큐 초기화
 q = deque()
 
-# 초기 상한 귤의 위치를 큐에 추가
+def push(x, y, num):
+    q.append((x, y))
+    visited[x][y] = num
+
+def bfs():
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if can_go(nx, ny, visited[x][y]):
+                push(nx, ny, visited[x][y] + 1)
+
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 2:
-            q.append((i, j))
-            visited[i][j] = 0
+            push(i, j, 0)
+            
 
-# BFS 수행
-while q:
-    x, y = q.popleft()
-    for dx, dy in zip(dxs, dys):
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 1 and visited[nx][ny] == -1:
-            visited[nx][ny] = visited[x][y] + 1
-            q.append((nx, ny))
+bfs()
 
-# 출력 준비
 for i in range(n):
     for j in range(n):
-        if arr[i][j] == 0:
-            visited[i][j] = -1
-        elif arr[i][j] == 1 and visited[i][j] == -1:
+        if arr[i][j] == 1 and visited[i][j] == -1:
             visited[i][j] = -2
-
-# 출력
-for row in visited:
-    print(" ".join(map(str, row)))
+print_visited()
