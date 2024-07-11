08226982dbdfba46d1b77186b0505b863ab8c408
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-11 16:47:54 +0900

    Title: 가장 많은 데이터, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240711/가장 많은 데이터/most-frequent-data.py
@@ -0,0 +1,14 @@
+n = int(input())
+d = dict()
+ans = 0
+for _ in range(n):
+    word = input()
+
+    if word not in d:
+        d[word] = 1
+    else:
+        d[word] += 1
+
+    ans = max(ans, freq[word])
+
+print(ans)
