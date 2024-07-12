6b667edd4c0587d4b63b8b3f39f5c1d8d9cd43cb
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-12 20:58:03 +0900

    Title: 낮은 지점들, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240712/낮은 지점들/lowest-points.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+count = dict()
+
+for _ in range(n):
+    x, y = tuple(map(int, input().split()))
+    if x in count:
+        if count[x] > y:
+            count[x] = y
+        else:
+            count[x] = y
+ans = 0
+for x, y in count.items():
+    ans += y
+
+print(ans)
