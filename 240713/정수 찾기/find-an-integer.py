278b4ba711fac4429f82c4f1ef0a433e578a83e2
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-13 20:34:11 +0900

    Title: 정수 찾기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240713/정수 찾기/find-an-integer.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr1 = list(map(int, input().split()))
+set1 = set(arr1)
+
+arr2 = list(map(int, input().split()))
+set2 = set(arr2)
+
+for a in arr2:
+    if a in set1:
+        print(1)
+    else:
+        print(0)
