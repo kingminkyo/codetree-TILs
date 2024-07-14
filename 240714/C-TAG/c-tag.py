42b22c83436cf7a8a14d9171078b0b2fff5e727b
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-14 20:12:12 +0900

    Title: C-TAG, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240714/C-TAG/c-tag.py
@@ -0,0 +1,28 @@
+n, m = tuple(map(int, input()))
+A = [input() for _ in range(n)]
+B = [input() for _ in range(n)]
+
+ans = 0
+s = set()
+
+
+def check(x, y, z):
+
+    s.clear()
+
+
+    for i in range(n):
+        s.add(A[i][x] + A[i][y] + A[i][z])
+
+    for i in range(n):
+        if (B[i][x] + B[i][y] + B[i][z]) in s:
+            return False
+        
+    return True
+
+for i in range(m):
+    for j in range(i+1. m):
+        for k in range(j+1, m):
+            if check(i, j, k): ans+= 1
+
+print(ans)
