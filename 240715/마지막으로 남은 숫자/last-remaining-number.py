1e5a6e8fc289b5e45030b5dabe48e01d5b6f5c0a
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-15 16:54:36 +0900

    Title: 마지막으로 남은 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,20 @@
+import heapq as hq
+n = int(input())
+arr = list(map(int, input().split()))
+pq = []
+for a in arr:
+    hq.heappush(pq, -a)
+
+
+
+
+while len(pq) >= 2:
+    fst = -hq.heappop(pq)
+    sec = -hq.heappop(pq)
+
+    cha = fst - sec
+
+    if cha != 0:
+        hq.heappush(pq, -cha)
+
+print(-pq[0])
