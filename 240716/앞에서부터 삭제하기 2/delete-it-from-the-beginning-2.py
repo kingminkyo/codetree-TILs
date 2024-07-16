8532b620b9a5fe42c9ad959c5ba781432d764fe6
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-16 12:47:54 +0900

    Title: 앞에서부터 삭제하기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240716/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240716/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,30 +1,26 @@
-from collections import deque 
 import heapq as hq 
 
 n = int(input())
 arr = list(map(int, input().split()))
 
+sum_val = 0
+max_avg = 0
+pq = []
 
-avg_max = 0
-
-for _ in range(n-2):
-    pq = []
-    arr.pop(0)
-    for a in arr:
-        hq.heappush(pq, a)
-    
-    idx = 0
-    pq_sum = 0
-    avg = 0
-    for i in range(1, len(pq)):
-        idx += 1 
-        pq_sum += pq[i]
-
-    avg = pq_sum / idx
-    avg_max = max(avg, avg_max)
-
-print(f"{avg_max:.2f}")
-
-
-# 3 1 9 2 7 
+sum_val += arr[n-1]
+hq.heappush(pq, arr[n-1])
 # k는 1에서 3까지 (1 ~ n-2)
+
+for i in range(n-2, 0, -1):
+
+    hq.heappush(pq, arr[i])
+    sum_val += arr[i]
+
+    sum_val -= pq[0]
+    avg = sum_val / (len(pq) - 1)
+
+    max_avg = max(avg, max_avg)
+    sum_val += pq[0]
+    # print(pq, avg, sum_val)
+
+print(max_avg)
