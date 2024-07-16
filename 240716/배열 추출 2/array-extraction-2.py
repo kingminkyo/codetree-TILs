ad9c0237d09ed97cd5296ea1bc2f1f0f13c9eef9
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-07-16 20:38:20 +0900

    Title: 배열 추출 2, Time: 731ms, Memory: 29MB, Status: Passed - CodeTree

--- 240716/배열 추출 2/array-extraction-2.py
+++ 240716/배열 추출 2/array-extraction-2.py
@@ -5,9 +5,6 @@ import heapq as hq
 max_pq, min_pq = [], []
 for _ in range(n):
     num = int(input())
-    
-
-    
 
     if num > 0:
         hq.heappush(max_pq, num)
@@ -18,8 +15,11 @@ for _ in range(n):
     elif num == 0:
         if len(max_pq) == 0 and len(min_pq) == 0:
             print(0)
+
+
         elif len(max_pq) == 0:
-            print(hq.heappop(min_pq))
+            print(-hq.heappop(min_pq))
+
         elif len(min_pq) == 0:
             print(hq.heappop(max_pq))
         elif min_pq[0] > max_pq[0]:
