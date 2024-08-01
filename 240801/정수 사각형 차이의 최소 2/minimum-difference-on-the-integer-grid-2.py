4e8776543633f9855df66ea9ec9a899df69b585b
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-01 21:10:12 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240801/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -0,0 +1,43 @@
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+max_arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+min_arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+dp = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+max_arr[0][0], min_arr[0][0], dp[0][0] = arr[0][0], arr[0][0], arr[0][0]
+
+for i in range(1, n):
+    max_arr[i][0] = max(max_arr[i-1][0], arr[i][0])
+    min_arr[i][0] = min(min_arr[i-1][0], arr[i][0])
+
+    max_arr[0][i] = max(max_arr[0][i-1], arr[0][i])
+    min_arr[0][i] = min(min_arr[0][i-1], arr[0][i])
+
+for i in range(1, n):
+    for j in range(1, n):
+        max_arr[i][j] = max(min(max_arr[i-1][j], max_arr[i][j-1]), arr[i][j])
+        min_arr[i][j] = min(min(min_arr[i-1][j], min_arr[i][j-1]), arr[i][j])
+
+for i in range(n):
+    for j in range(n):
+        dp[i][j] = max_arr[i][j] - min_arr[i][j]
+
+print(dp[n-1][n-1])
+
+
+# print(max_arr)
+# print(min_arr)
