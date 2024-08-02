623e005cc6ba5c98cc665bb314d33320a7e5f897
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-02 19:56:22 +0900

    Title: 1차원 젠가, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240802/1차원 젠가/jenga-1d.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+block = []
+
+for _ in range(n):
+    block.append(int(input()))
+
+def arrange():
+    global block
+    temp = []
+    for b in block:
+        if b != 0:
+            temp.append(b)
+    block = temp
+
+
+for _ in range(2):
+    start, end = tuple(map(int, input().split()))
+    start, end = start-1, end-1 
+    for i in range(start, end+1):
+        block[i] = 0
+
+    
+    arrange()
+    print(block)
+
+print(len(block))
+for b in block:
+    print(b)
