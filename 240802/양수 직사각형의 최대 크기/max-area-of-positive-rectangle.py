20ef001dd4fe69999e13349c2c40118b38864b03
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-02 13:42:26 +0900

    Title: 양수 직사각형의 최대 크기, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240802/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -0,0 +1,31 @@
+n, m = tuple(map(int, input().split()))
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+def make_rec(x1, y1, x2, y2):
+
+    result = 0
+
+    for i in range(x1, x2+1):
+        for j in range(y1, y2+1):
+            result += arr[i][j]
+
+            if arr[i][j] < 0:
+                return 0
+
+    return result
+
+final = 0 
+rec_size =0
+
+for i in range(n):
+    for j in range(n):
+        for k in range(i, n):
+            for l in range(j, n):
+                if make_rec(i, j, k, l) > 1:
+                    rec_size = (k-i+1) * (l-j+1)
+                if rec_size > final:
+                    final = rec_size
+print(final)
