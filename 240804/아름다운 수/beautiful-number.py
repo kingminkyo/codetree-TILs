b08a197e02c22ac2ad46a34954a43867378614ac
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-04 08:57:55 +0900

    Title: 아름다운 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240804/아름다운 수/beautiful-number.py
@@ -0,0 +1,42 @@
+n = int(input())
+
+
+ans = []
+
+def check_beautiful():
+    cnt = 1 
+    for i in range(n-1):
+        if arr[i] == ans[i+1]:
+            cnt += 1
+        else:
+            if cnt % ans[i] == 0:
+                cnt = 1
+            else:
+                return False
+    if cnt % ans[n-1] != 0:
+        return False
+    
+    return True
+
+
+def print_ans():
+    for a in ans:
+        print(a, end="")
+    print()
+
+
+
+
+def choose(num):
+    if num ==  n:
+        if check_beautiful():
+            print_ans()
+
+        return
+
+    for i in range(1, 5):
+        ans.append(i)
+        choose(num+1)
+        ans.pop()
+
+choose(0)
