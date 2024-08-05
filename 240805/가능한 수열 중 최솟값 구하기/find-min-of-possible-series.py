c85c83b5e745bfff0412a8c13a5d621b6c20c2c1
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-05 21:38:34 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240805/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 240805/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -9,6 +9,30 @@ def print_ans():
 
 key = False
 
+def is_okay():
+    if key == True:
+        return 
+    length = 1
+    while True:
+
+        start1, end1 = len(ans) - length, len(ans) - 1
+        start2, end2 = start1 - length, start1 - 1
+
+        if start2 < 0:
+            break
+
+        if ans[start1:end1+1] == ans[start2:end2+1]:
+            return False
+        
+        length+=1
+    
+    return True
+
+
+
+
+
+
 def choose(num):
     global key 
 
@@ -19,22 +43,9 @@ def choose(num):
         return 
 
     for i in range(4, 7):
-        if key == True:
-            break 
-        if num >= 1:
-            if ans[-1] == i:
-                continue
-        if num >= 3:
-            if ans[-3] == ans[-1] and ans[-2] == i:
-                continue
-        if num >= 5:
-            if ans[-5] == ans[-2] and ans[-4] == ans[-1] and ans[-3] == i:
-                continue
-                
-            
-
-        ans.append(i)
-        choose(num+1)
+        ans.append(i)   
+        if is_okay():
+            choose(num+1)
         ans.pop()
 
 choose(0)
