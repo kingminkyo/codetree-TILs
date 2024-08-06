e375c4be458dddb6943c4d5b4574196423f913aa
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-06 23:28:59 +0900

    Title: 안전 지대, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/안전 지대/comfort-zone.py
@@ -0,0 +1,75 @@
+n, m = tuple(map(int, input().split()))
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+def reset_visited():
+    for i in range(n):
+        for j in range(m):
+            visited[i][j] = 0
+
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<m
+
+def can_go(x, y, k):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] == 1 :
+        return False
+    return True
+
+count = 0 
+def dfs(x, y, k):
+    
+    dxs, dys = [0, 0, 1, 0, -1], [0, 1, 0, -1, 0]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+        
+        if can_go(nx, ny, k):
+            visited[nx][ny] = 1
+            dfs(nx, ny, k)
+
+max_num = 0
+def print_visited():
+    for i in range(n):
+        for j in range(m):
+            print(visited[i][j], end=" ")
+        print()
+    print()
+        
+for i in range(n):
+    for j in range(m):
+        max_num = max(max_num, arr[i][j])
+
+result = 0
+for k in range(1, max_num+1):
+    count = 0
+    reset_visited()
+
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] <= k:
+                visited[i][j] = 1
+
+
+    # print_visited()
+
+    for i in range(n):
+        for j in range(m):
+            if can_go(i, j, k):
+                dfs(i, j, k)
+                count += 1 
+
+    # print("k는 ", k, "zone은 ", count )
+    # print_visited()
+    result = max(result, count)
+
+print(result )
