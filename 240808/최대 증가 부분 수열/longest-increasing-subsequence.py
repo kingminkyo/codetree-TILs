4f4207f3fe052883e04fe20cf8e3d1795d22e5db
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-08 01:21:36 +0900

    Title: 최대 증가 부분 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240808/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240808/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,12 +1,12 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [ -1 for _ in range(n) ]
+dp = [ -1 for _ in range(n+1) ]
 dp[0] = 1
 
-for i in range(1, n):
+for i in range(1, n+1):
     
-    for j in range(0, i):
+    for j in range( i):
 
 
 
