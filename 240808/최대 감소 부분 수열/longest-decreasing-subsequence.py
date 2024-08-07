01a54ed135d5e855697217e08e97eafe8550e8c4
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-08-08 07:25:23 +0900

    Title: 최대 감소 부분 수열, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 240808/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -1,13 +1,14 @@
+import sys
+
 n = int(input())
-arr = list(map(int, input().split()))
-dp = [-1 for _ in range(n)]
+arr = [sys.maxsize] + list(map(int, input().split()))
+dp = [-1 for _ in range(n+1)]
 
-dp[0] = 1
-for i in range(1, n):
-
+dp[0] = 0
+for i in range(1, n+1):
     for j in range(0, i):
 
         if arr[i] < arr[j]:
             dp[i] = max(dp[i], dp[j]+1)
-
+# print(dp)
 print(max(dp))
