75cba17df6d38fca87ba44c76987b29811507dd3
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-03 14:27:32 +0900

    Title: 아름다운 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241003/아름다운 수/beautiful-number.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n = 0 ; 
+vector<int> arr;
+
+void printArr(){
+    for (int i=0; i<arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+bool isBeautiful(){
+    for (int i=0; i<arr.size(); i += arr[i]){
+
+        // if(i + arr[i] - 1 >= n)
+        //     return false;
+        // printArr();
+        for (int j = i; j < i+arr[i]; j++){
+            if (arr[j] != arr[i]){
+                return false;
+            }
+        }
+
+
+
+    }
+
+    printArr();
+    return true;
+}
+
+
+
+
+
+int result = 0; 
+
+void choose(int cnt){
+
+    if (cnt == n){
+        // printArr();
+        if (isBeautiful())
+            result++; 
+        return ;
+    }
+
+    for(int i=1; i<5; i++){
+        arr.push_back(i);
+        choose(cnt+1);
+        arr.pop_back();
+    }
+}
+
+
+
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n ; 
+    choose(0);
+
+    cout << result; 
+    return 0;
+}
