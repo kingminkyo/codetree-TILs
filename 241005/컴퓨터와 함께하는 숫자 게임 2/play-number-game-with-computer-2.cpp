53eab00b907a169d0112809c5157c23241d5d16b
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-05 04:42:30 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241005/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
+++ 241005/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -1,57 +1,46 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-
-vector<long long> arr; 
 long long m; 
 
+// target을 맞출 때까지의 이진 탐색 횟수를 반환
 long long binary_search(long long target){
-    long long left = 0;
-    long long right = m-1;
-
+    long long left = 1; // 1부터 시작
+    long long right = m; // m까지
     long long time = 0;
 
     while(left <= right){
         time += 1;
-        long long mid = (left + right) / 2;
+        long long mid = left + (right - left) / 2;
 
-        if (arr[mid] == target)
-            return time;
-        if (arr[mid] > target)
+        if (mid == target) {
+            return time;  // 정답을 맞췄을 때 시간을 반환
+        }
+        if (mid > target)
             right = mid - 1;
         else
             left = mid + 1;
-
     }
-
+    return time;  // 이론적으로는 여기까지 오지 않음
 }
 
-
-
-
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 입력
     cin >> m; 
-
-    for(long long i = 1; i <= m; i++){
-        arr.push_back(i);
-    }
 
     int a, b;
     cin >> a >> b;
 
     long long max_t = 0;
-    long long min_t = 1000000; 
+    long long min_t = 100; // log2(m)으로 충분함
+
     for(long long i = a ; i <= b ; i++){
         long long time = binary_search(i);
         max_t = max(max_t, time);
         min_t = min(min_t, time);
     }
 
-    cout << min_t << " " << max_t ;
-
-
+    cout << min_t << " " << max_t << endl;
 
     return 0;
 }
