61e9c158efb5c88b6c3f12a65f0e8aadf962624c
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-04 21:58:33 +0900

    Title: 특별한 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/특별한 문자/special-character.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <unordered_map> 
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string word;
+    cin >> word;
+
+    unordered_map<char, int> freq;
+
+    for(char s : word)
+        freq[s]++;
+    
+    bool key = false;
+    for(char s : word){
+        if (freq[s] == 1){
+            cout << s;
+            key = true;
+            break;
+        }
+
+    if (!key)
+        cout << "None"; 
+
+    }
+    
+
+    return 0;
+}
