f2cf1083526194cfd2925da894e637dc755b2482
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-04 20:18:39 +0900

    Title: 두 수의 합, Time: 108ms, Memory: 6MB, Status: Passed - CodeTree

---
+++ 241004/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+#define MAX_N 100000
+
+int n, k;
+int arr[MAX_N];
+unordered_map<int, int> count;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n >> k; 
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int ans = 0;
+
+    for(int i = 0; i < n; i++){
+        int diff = k - arr[i];
+        ans += count[diff];
+
+        count[arr[i]]++;
+    }
+
+    cout << ans;
+    return 0;
+}
