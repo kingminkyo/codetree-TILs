1bd6eab0d6fad8ef9c3bd2747986ad7b39400b5e
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-04 20:05:05 +0900

    Title: 대응되는 수와 문자, Time: 124ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 241004/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_map>
+
+#define MAX_N 100000
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    unordered_map<string, int> map1;
+    unordered_map<int, string> map2;
+
+    int n, m; 
+    string s; 
+
+    cin >> n >> m; 
+
+    for (int i = 1; i <= n; i++){
+        cin >> s;
+
+        map1[s] = i;
+        map2[i] = s;
+    }
+
+    for (int i = 1; i <= m; i++){
+        cin >> s;
+
+        if (s >= "0" && s <= "9"){
+            cout << map2[stoi(s)] << endl;
+        }
+        else{
+            cout << map1[s] << endl;
+        }
+    }
+
+    return 0;
+}
