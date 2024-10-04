5d4367599ffa0c313021b63227757f5bedb6630a
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-05 00:13:31 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241005/개인정보/personal-info.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 5
+
+class Student{
+    public:
+        string name;
+        int height;
+        double weight;
+
+        Student(string name, int height, double weight){
+            this->name = name;
+            this->height = height;
+            this->weight = weight;
+        }
+        Student(){}
+};
+
+bool cmp_name(Student a, Student b){
+    return a.name < b.name;
+}
+
+bool cmp_height(Student a, Student b){
+    return a.height > b.height;
+}
+
+Student students[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i = 0; i < MAX_N; i++){
+        string name;
+        int height;
+        double weight;
+
+        cin >> name >> height >> weight;
+        students[i] = Student(name, height, weight);
+    }
+
+    sort(students, students + MAX_N, cmp_name);
+
+    cout << "name" << endl;
+    for (int i = 0; i < MAX_N; i++){
+        cout << students[i].name << " " << students[i].height << " " << students[i].weight << endl; 
+    }
+
+
+    sort(students, students + MAX_N, cmp_height);
+
+    
+    cout << endl << "height" << endl; 
+    for (int i = 0; i < MAX_N; i++){
+        cout << students[i].name << " " << students[i].height << " " << students[i].weight << endl; 
+    }
+
+    return 0;
+}
