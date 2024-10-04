e38bd53fe5b22ce9bc57f73297bca3947df0489f
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-04 21:10:09 +0900

    Title: 자주 등장한 top k 숫자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+
+using namespace std;
+
+#define MAX_N 100000
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k; 
+
+    unordered_map<int, int> freq;
+    vector<pair<int, int>> v;
+    int arr[MAX_N];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        freq[arr[i]] += 1;
+    }
+
+    for(unordered_map<int, int>::iterator it = freq.begin(); it != freq.end(); it++){
+        v.push_back(make_pair(it->second, it->first));
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < k; i++){
+        cout << v[i].second << " "; 
+    }
+
+    // for(int i = (int)v.size() - 1; i >= (int)v.size() - k; i--)
+    // cout << v[i].second << " ";
+
+
+
+    return 0;
+}
