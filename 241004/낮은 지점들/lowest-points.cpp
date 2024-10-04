355c91835178c7718f27076cdaf5ea3b1f1bc720
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-04 22:14:57 +0900

    Title: 낮은 지점들, Time: 53ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 241004/낮은 지점들/lowest-points.cpp
+++ 241004/낮은 지점들/lowest-points.cpp
@@ -3,29 +3,30 @@
 
 using namespace std;
 
+
+
 int main() {
+    // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
 
     unordered_map<int, int> point; 
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++){
         int x, y;
         cin >> x >> y;
 
-        // find를 사용하여 값이 존재하는지 확인
-        if (point.find(x) == point.end()) {
-            point[x] = y;  // 값이 없으면 추가
-        } else if (point[x] > y) {
-            point[x] = y;  // 기존 값보다 작은 경우 갱신
-        }
+        if (point.find(x) == point.end())
+            point[x] = y;
+        else if(point.find(x) != point.end() && point[x] > y)
+            point[x] = y;
     }
-
+    
     int ans = 0; 
-    for (unordered_map<int, int>::iterator it = point.begin(); it != point.end(); it++) {
-        ans += it->second;
-    }
-
-    cout << ans;
+    for (unordered_map<int, int>::iterator it = point.begin();
+         it != point.end(); it++){
+            ans += it->second;
+         }
+    cout << ans; 
     return 0;
 }
