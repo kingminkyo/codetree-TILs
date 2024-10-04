6745a03da648a20588c76a17d56b7b828e176c89
Kang Minkyo <98573877+kingminkyo@users.noreply.github.com>
2024-10-04 15:59:25 +0900

    Title: n개의 점 중 m개 고르기, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241004/n개의 점 중 m개 고르기/choose-m-out-of-n-points.cpp
+++ 241004/n개의 점 중 m개 고르기/choose-m-out-of-n-points.cpp
@@ -1,49 +1,87 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-#include <cmath>  // sqrt 함수 사용을 위해 포함
+#include <cmath>
 
 using namespace std;
 
-int n;
+int n, m;
 vector<pair<int, int>> lines; 
+vector<pair<int, int>> selected; 
 
-// 두 점 사이의 거리 제곱합을 계산하는 함수
-int squared_distance(pair<int, int> p1, pair<int, int> p2) {
+
+int ucl_dist(pair<int, int> p1, pair<int, int> p2){
     int x1, y1;
     tie(x1, y1) = p1;
 
     int x2, y2;
     tie(x2, y2) = p2;
 
-    return pow((x1 - x2), 2) + pow((y1 - y2), 2); 
+    return pow((x1-x2), 2) + pow((y1-y2), 2); 
 }
 
-// 가장 가까운 두 점 사이의 거리 제곱합을 구하는 함수
-int calc() {
-    int result = INT_MAX;  // 비교를 위한 매우 큰 값
-    for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
-            result = min(result, squared_distance(lines[i], lines[j]));
+
+
+
+
+int calc(){
+    int result = 10000; 
+    for(int i = 0; i < m; i++){
+        for(int j = i+1; j < m ; j++){
+            result = min(result, ucl_dist(selected[i], selected[j]));
         }
     }
     return result; 
 }
 
-int main() {
-    // 입력 처리
-    cin >> n;  // m은 필요 없으므로 제거
+void selected_print(){
+    for(auto a : selected){
+        cout << a.first << "/" << a.second << " ";
+    cout << endl;
+    
+    }
+}
 
-    for(int i = 0; i < n; i++) {
+int result = 10000; 
+
+void choose(int idx, int cnt){
+    if (cnt == m){
+        // selected_print();
+        // cout << "--" << endl; 
+        result = min(calc(), result);
+        return; 
+    }
+
+    if(idx == n)
+        return;
+
+    selected.push_back(lines[idx]);
+    choose(idx+1, cnt+1);
+    selected.pop_back();
+
+    choose(idx+1, cnt); 
+
+
+
+
+}
+
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m; 
+
+    for(int i = 0; i < n; i++){
         int a, b;
         cin >> a >> b;
+
         lines.push_back(make_pair(a, b));
     }
 
-    int min_squared_dist = calc();  // 최단 거리 제곱합 계산
-    cout << "최단 거리 제곱합: " << min_squared_dist << endl;
 
-    cout << "최단 거리: " << sqrt(min_squared_dist) << endl;  // 제곱근 계산하여 출력
+    choose(0, 0);
 
+    cout << result; 
     return 0;
 }
